0e07ae9996243686ca6d2c8cfbcf98e2e65fefb3
Andrea Vaghi <andrea.vaghi@gmail.com>
2014-06-10 08:48:46 +0200

    gaecosmetica

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 var Gae = {};
 Gae.Ui = (function() {
 
-    var imageList = [
-    	{ src: 'images/01.jpg' }, 
-    	{ src: 'images/02.jpg' },
-    	{ src: 'images/03.jpg' },
-    	{ src: 'images/04.jpg' },
-    	{ src: 'images/05.jpg' },
-    	{ src: 'images/06.jpg' },
-    	{ src: 'images/07.jpg' },
-    	{ src: 'images/08.jpg' }
+    var slider = [
+    	{ src: 'images/slider/01.jpg' }, 
+    	{ src: 'images/slider/02.jpg' },
+    	{ src: 'images/slider/03.jpg' },
+    	{ src: 'images/slider/04.jpg' },
+    	{ src: 'images/slider/05.jpg' }
     ];
 
-    function init() {
-        setupSlider(imageList);
+    function init(imageList) {
+        var setup = (imageList.length > 1) ? setupSlider(imageList) : setupBackground(imageList);
+        secondLevelMenu();
     }
 
     function setupSlider(imageList) {
@@ -22,11 +20,24 @@ Gae.Ui = (function() {
         })();
     }
 
+    function setupBackground(imageList) {
+        $.vegas({
+            src: imageList[0]
+        });
+    }
+
+    function secondLevelMenu() {
+        $('a.second-level').off('click').on('click', function(e) {
+            e.preventDefault();
+            var section = $(this).attr('data-section');
+            $('nav.second-level').slideDown(500, function(){
+                $(this).find('ul.' + section).show();
+            });
+        });
+    }
+
     return {
+        slider: slider,
         init: init
     }
 }());
-
-$(function() {
-    Gae.Ui.init();
-});
